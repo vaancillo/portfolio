687b7a05c8d38be6c823f4c8a7c62038000a1ea5
vancillo <yaredbane@gmail.com>
2023-10-06 23:46:36 -0600

    change the title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next'
 
 
 export const metadata: Metadata = {
-  title: 'Portfolio TarreDev - NextJS 13',
+  title: 'Portfolio JaredDev - NextJS 13',
   description: 'Portfolio animated ',
 }
 
